6a88c04cfdfeb43415d2ff7febd70b18400ffef8
zahrul100 <39825311+zahrul100@users.noreply.github.com>
2020-02-28 19:18:40 +0700

    Create udpfileserver.py

---
+++ Tugas 2/udpfileserver.py
@@ -0,0 +1,22 @@
+import socket
+
+
+SERVER_IP = '10.151.254.214'
+SERVER_PORT = 5005
+NAMAFILE='coba.png'
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((SERVER_IP, SERVER_PORT))
+
+fp = open(NAMAFILE,'wb+')
+ditulis=0
+
+counter=0
+while True:
+    data, addr = sock.recvfrom(1024)
+    counter=counter+len(data)
+    print(addr," blok ", counter,"panjang : ",len(data), data)
+    fp.write(data)
+
+
+fp.close()
